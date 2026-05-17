b428df745b1bba59bd160b3a69bab538c5083f03
agussnv <novielloagustin@gmail.com>
2026-05-17 19:08:43 +0200

    wake word v0.1

--- prueba_mic_pi.py
+++ prueba_mic_pi.py
@@ -3,6 +3,8 @@ import sounddevice as sd # Captura el sonido del microfono y lo reproduce
 import numpy as np # Librería matemática para arrays. Cada número es la amplitud de onda
 import scipy.io.wavfile as wav # Guarda el array de números como un archivo .wav
 import pygame # Para reproducir el sonido la voz. Forma más sencilla de hacerlo con PyGame.
+import openwakeword # para activarse mediante la wake word
+import time
 import tempfile
 
 os.environ["SDL_AUDIODRIVER"] = "alsa"
@@ -14,15 +16,51 @@ mipo_hablando = False
 
 SAMPLE_RATE=44100 # mediciones por SEGUNDO que se hace del sonido
 DEVICE=0
+CHANNELS=1
 SILENCIO_UMBRAL=100
 SILENCIO_SEGUNDOS=2
 TIMEOUT=10
+
+_base = os.path.dirname(openwakeword.__file__)
+WAKE_WORD_PATH = os.path.join(_base, "resources", "models", "alexa_v0.1.onnx")
+WAKE_WORD_UMBRAL=0.5
 
 def hay_voz(audio_chunk): # audio_chunk son 100ms de audio, un conjunto de números que representa la onda de sonido
     return np.abs(audio_chunk).mean() > SILENCIO_UMBRAL
     # np.abs(array_numerico) transforma todos los números a positivos. Ya que nos interesa su amplitud, no su signo
     # .mean() calcula la media entre todos estos números. Si están por debajo de SILENCIO_UMBRAL, es que no hay sonido
     # por eso luego se le suma 0.1 (100ms) a la cantidad de audio sin voz y va almacenando esas ondas en fragmentos.
+
+def esperar_wake_word():
+    global modelo_wakeword
+    modelo_wakeword = openwakeword.Model(wakeword_model_paths=[WAKE_WORD_PATH])
+    
+    print("Durmiendo... Di 'Alexa' para activar")
+    
+    ultimo_activado=0
+    COOLDOWN=5
+    
+    with sd.InputStream(
+        samplerate=SAMPLE_RATE,
+        channels=CHANNELS,
+        dtype=np.int16,
+        device=DEVICE,
+        blocksize=1280
+    ) as stream:
+        
+        while True:
+            chunk, _ = stream.read(1280)
+            chunk = chunk.flatten()
+                
+            prediccion = modelo_wakeword.predict(chunk)
+            score = list(prediccion.values())[0]
+            print("SCORE: " + score)
+        
+            ahora = time.time()
+            if score > WAKE_WORD_UMBRAL and (ahora - ultimo_activado) > COOLDOWN:
+                ultimo_activado = ahora
+                print("ACTIVADO")
+                return
 
 def escuchar() -> str: # -> str es solamente de tipo informativo para quien lea el documento, no afecta funcionamiento.
     print("Escuchando...")
@@ -68,4 +106,4 @@ def escuchar() -> str: # -> str es solamente de tipo informativo para quien lea 
     
     os.remove(archivo)
 
-escuchar()
+esperar_wake_word()
